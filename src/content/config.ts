873a0da222ea52d5b80076b20f6270ce9dafb4b2
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-03-14 16:27:02 +0200

    added: redesigned

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,66 @@
 import { defineCollection, z } from "astro:content";
-
+const team = defineCollection({
+  schema: z.object({
+    name: z.string(),
+    role: z.string(),
+    intro: z.string(),
+    education: z.array(z.string()),
+    experience: z.array(z.string()),
+    avatar: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+  }),
+});
+const services = defineCollection({
+  schema: z.object({
+    service: z.string(),
+    description: z.string(),
+  }),
+});
+const work = defineCollection({
+  schema: z.object({
+    company: z.string().optional(),
+    year: z.string().optional(),
+    client: z.string().optional(),
+    work: z.string(),
+    credits: z
+      .array(
+        z.object({
+          name: z.string(),
+          role: z.string(),
+        })
+      )
+      .optional(),
+    logo: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+  }),
+});
+const infopages = defineCollection({
+  schema: z.object({
+    page: z.string(),
+    pubDate: z.date(),
+  }),
+});
 const postsCollection = defineCollection({
     schema: z.object({
       title: z.string(),
       pubDate: z.date(),
       description: z.string(),
       author: z.string(),
-
       image: z.object({
         url: z.string(),
         alt: z.string()
       }),
-
       tags: z.array(z.string())
     })
  });
-
 export const collections = {
+  team: team,
+  work: work,
+  services: services,
+  infopages : infopages,
   posts: postsCollection,
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,103 +4,65 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+
+      sans: ["Clash Display", ...defaultTheme.fontFamily.sans],
+    },
+
+
+    colors: {
+      white: colors.white,
+      black: colors.black,
+      gray: colors.gray,
+      zinc: colors.zinc,
+      transparent: colors.transparent,
+      green: {
+        50: "hsl(104, 65%, 97%)",
+        100: "hsl(111, 68%, 93%)",
+        200: "hsl(112, 66%, 85%)",
+        300: "hsl(111, 64%, 73%)",
+        400: "hsl(112, 57%, 58%)",
+        500: "hsl(112, 58%, 45%)",
+        600: "hsl(112, 63%, 35%)",
+        700: "hsl(112, 60%, 29%)",
+        800: "hsl(113, 53%, 24%)",
+        900: "hsl(113, 51%, 20%)",
+        950: "hsl(115, 65%, 10%)",
+      },
+      blue: {
+       '50': 'hsl(208, 100%, 97%)',
+    '100': 'hsl(206, 94%, 94%)',
+    '200': 'hsl(202, 94%, 86%)',
+    '300': 'hsl(201, 97%, 74%)',
+    '400': 'hsl(200, 94%, 57%)',
+    '500': 'hsl(201, 89%, 48%)',
+    '600': 'hsl(203, 99%, 39%)',
+    '700': 'hsl(203, 98%, 32%)',
+    '800': 'hsl(203, 91%, 27%)',
+    '900': 'hsl(204, 82%, 24%)',
+    '950': 'hsl(206, 80%, 16%)',
+      },
+       violet: {
+      '50': 'hsl(255, 100%, 98%)',
+    '100': 'hsl(257, 91%, 95%)',
+    '200': 'hsl(257, 100%, 92%)',
+    '300': 'hsl(259, 97%, 85%)',
+    '400': 'hsl(261, 95%, 76%)',
+    '500': 'hsl(264, 92%, 65%)',
+    '600': 'hsl(268, 86%, 58%)',
+    '700': 'hsl(270, 72%, 50%)',
+    '800': 'hsl(269, 71%, 42%)',
+    '900': 'hsl(270, 70%, 35%)',
+    '950': 'hsl(268, 74%, 23%)',
+      },
+    },
     extend: {
-      animation: {
-        marquee: "marquee 25s linear infinite",
-        marquee2: "marquee2 25s linear infinite",
-        scroller3: "scroller3 25s linear infinite",
-        "spin-slow": "spin 4s linear infinite",
-        "spin-slower": "spin 6s linear infinite",
-        "spin-reverse": "spin-reverse 1s linear infinite",
-        "spin-reverse-slow": "spin-reverse 4s linear infinite",
-        "spin-reverse-slower": "spin-reverse 6s linear infinite",
-        scroller: "scroller 15s linear infinite",
-        scroller2: "scroller2 20s linear infinite",
-        "fade-in": "fade-in 0.5s linear forwards",
-      },
-      keyframes: {
-        marquee: {
-          "0%": {
-            transform: "translateX(0%)",
-          },
-          "100%": {
-            transform: "translateX(-100%)",
-          },
-        },
-        marquee2: {
-          "0%": {
-            transform: "translateX(100%)",
-          },
-          "100%": {
-            transform: "translateX(0%)",
-          },
-        },
-        scroller: {
-          "0%": {
-            transform: "translateY(10em)",
-          },
-          "100%": {
-            transform: "translateY(-14em)",
-          },
-        },
-        scroller2: {
-          "0%": {
-            transform: "translateY(10em)",
-          },
-          "100%": {
-            transform: "translateY(-14em)",
-          },
-        },
-        "fade-in": {
-          from: {
-            opacity: 0,
-          },
-          to: {
-            opacity: 1,
-          },
-        },
-        scroller3: {
-          "100%": {
-            transform: "translateY(-50%)",
-          },
-        },
-        "spin-reverse": {
-          to: {
-            transform: "rotate(-360deg)",
-          },
-        },
-      },
-      backgroundImage: (theme) => ({
-        gradientTopRight:
-          "linear-gradient(229.7deg,#fdfdfe 14.85%,#ffbb88 20.61%,#ff7cba 26.17%,#887bff 31.93%,#03c7f6 46.54%,#0F1014 54.36%)",
-          gradientBottomLeft:
-          "linear-gradient(47deg, #FDFDFE 10.68%, #FB8 17.61%, #FF7CBA 24.3%, #887BFF 31.23%, #03C7F6 48.82%, #0F1014 58.23%)",
-          gradientToTop:
-          "linear-gradient(0deg, #FFF -2.39%, #FB8 7.4%, #FF7CBA 16.83%, #887BFF 26.61%, #03C7F6 51.42%, #0F1014 64.69%)",
-      }),
-      boxShadow: {
-        thick: "0px 7px 32px rgb(0 0 0 / 35%);",
-        inset:
-          "inset 6px 84px 79px -40px hsla(0,0%,100%,.025), inset 0 -4px 1px -3px hsla(0,0%,100%,.25), inset 0 4px 1px -3px hsla(0,0%,100%,.25);",
-      },
-      colors: {
-        black: "#0f1014",
-        ebony: "#1a1c23",
-        mirage: "#27292D",
-       
-      },
       borderRadius: {
         "4xl": "2rem",
         "5xl": "3rem",
         "6xl": "5rem",
       },
-      fontFamily: {
-        sans: ["Inter", ...defaultTheme.fontFamily.sans],
-      },
     },
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-  ],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
